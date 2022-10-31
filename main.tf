20acaa1ff9b1e70b2d25abd5d42a48e197681182
Khalid <33844176+ahmadzaikk@users.noreply.github.com>
2022-10-31 14:00:01 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "aws_codecommit_repository" "this" {
-  repository_name = join("-", [var.repository_name, "repo"])
+  repository_name = join("-", [var.repository_name])
   description     = var.description
   default_branch  = var.default_branch
 
