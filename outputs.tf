88dadf38bd0714a69aa368fbfe09ce540322d010
George Holbert <gholbert@ucop.edu>
2023-11-21 09:38:01 -0800

    never mind on cross_account_role_arn output (not needed here)

--- outputs.tf
+++ outputs.tf
@@ -22,7 +22,3 @@ output "name" {
   description = "The name for the repository"
   value       = var.repository_name
 }
-
-output "cross_account_role_arn" {
-  value = var.repo_cross_account_role_name != "" ? aws_iam_role.cross_account[0].arn : null
-}
